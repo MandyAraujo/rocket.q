c8ac60035c9c63a28666a2eb309d284d065b4b65
Amanda Araujo <amanda.araujod3v@gmail.com>
2022-05-09 23:36:42 -0300

    add express and nodejs

---
+++ public/scripts/main.js
@@ -0,0 +1,35 @@
+import  Modal  from './modal.js'
+
+const modal = Modal()
+
+const modalTitle = document.querySelector('.modal h2')
+const modalDEscription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+const checkButtons = document.querySelectorAll(".actions a.check")
+
+checkButtons.forEach(button => {
+
+    button.addEventListener("click", handleClick)
+})
+
+const deleteButton = document.querySelectorAll(".actions a.delete")
+
+deleteButton.forEach(button => {
+    button.addEventListener("click", (event) => handleClick(event, false))
+})
+
+function handleClick(event, check = true){
+
+    event.preventDefault()
+
+    const text = check ? "Marcar como lida" : "Excluir"
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDEscription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()} `
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+
+
+    modal.open()
+}
