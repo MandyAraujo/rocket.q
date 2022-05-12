216403b2297376ba1007d5135a52613e4d299ea7
Amanda Araujo <amanda.araujod3v@gmail.com>
2022-05-11 23:43:46 -0300

    delete questions

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -5,14 +5,27 @@ const Database = require('../db/config')
 module.exports = {
     
 
-    index(req, res){
+    async index(req, res){
+        const db = await Database()
         const roomId = req.params.room
         const questionId = req.params.question
         const action = req.params.action
         const password = req.body.password
 
-        console.log(`room = ${roomId}, questionId = ${questionId}
-        action = ${action}, password = ${password}`)
+        // verificar se a senha está correta
+
+        const verifyRoom = await db.get(`SELECT * FROM rooms WHERE pass = ${password}`)
+
+        if(verifyRoom.pass == password){
+            if(action == "delete"){
+                await db.run(`DELETE FROM questions WHERE id = ${questionId}`)
+            }
+        }else if(action == "check"){
+            await db.run (`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
+        }
+
+        res.redirect(`/room/${roomId}`)
+        
 
     },
 
